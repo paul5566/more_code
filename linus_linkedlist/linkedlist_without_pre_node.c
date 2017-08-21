0e84168f8d1d45cde6188cb8ac2fe0ff71b18e53
Phil <soso7885@hotmail.com>
2017-08-21 16:49:55 +0800

    more comment

--- linus_linkedlist/linkedlist_without_pre_node.c
+++ linus_linkedlist/linkedlist_without_pre_node.c
@@ -8,11 +8,13 @@ struct node{
 
 void show_list(struct node *head)
 {
-	while(head){
-		printf("%d -> ", head->value);
-		if(head->next == NULL)
+	struct node *tmp = head;
+
+	while(tmp){
+		printf("%d -> ", tmp->value);
+		if(tmp->next == NULL)
 			printf("NULL");
-		head = head->next;
+		tmp = tmp->next;
 	}
 	printf("\n");
 }
@@ -46,8 +48,35 @@ int main(int argc, char **argv)
 			 * *ppHead = head->next->....(based on passing)
 			 * (head->next->...) = (head->next->next...)->next
 			 * thus jump over the remove node
+			 *
+			 * eg: remove 3
+			 *
+			 * -----    -----    -----    -----    -----
+			 * | 1 | -> | 2 | -> | 3 | -> | 4 | -> | 5 |
+			 * -----    -----    -----    -----    -----
+			 *                     ^
+			 *                     |
+			 * @(*ppHead)----------|
+			 *
+			 * *ppHead is point to node(3), which means *ppHead = head->next->next(2)->next(3),
+			 * then susbsitute to node(4), such like copy, copy node(4) to node(3), so the
+			 * next of node(2) is node(4)
 			*/
 			(*ppHead) = (*ppHead)->next;
+			/*
+			 * After that:
+			 * -----    -----    -----    
+			 * | 1 | -> | 2 | _  | 3 |  
+			 * -----    ----- |  -----    
+			 *                |        
+			 *                |  -----    -----
+			 *                |->| 4 | -> | 5 |
+			 *                   -----    -----
+			 *                     ^
+			 *                     |
+			 * @(*ppHead)----------|
+			 */
+			break;
 		}else{
 			ppHead = &((*ppHead)->next);
 		}
